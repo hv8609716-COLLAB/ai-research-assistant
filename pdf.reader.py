42e5243609bfa40453a54cce2f9832755309ec00
HARSH VARDHAN <hv8609716@gmail.com>
2026-07-02 12:14:17 +0530

    Add PDF reading functionality using pypdf

--- pdf.reader.py
+++ pdf.reader.py
@@ -1,1 +1,14 @@
+from pypdf import PdfReader
+
+
+def read_pdf(path):
+
+    reader = PdfReader(path)
+
+    text = ""
+
+    for page in reader.pages:
+        text += page.extract_text()
+
+    return text
 
